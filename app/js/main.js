bc7b2093f72fef2d39de151c0e3dc0eae588d099
morj <ulanovus@gmail.com>
2021-11-13 17:05:48 +0300

    added header and hero setions

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+
+  $('.hero__inner').slick({
+    nextArrow: '<button type="button" class="slick-next"><img src="images/slider/arrow-right.svg" alt="стрелка вправо"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/slider/arrow-left.svg" alt="стрелка влево"></button>'
+  });
+
+
+});
